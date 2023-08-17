000d702021b6a19d0687dc4508a28f9127938fd8
Dominik K <dominik@koch-bautechnik.de>
2023-08-17 19:26:37 +0200

    feat(cleanup): clean some code

--- commands/test.js
+++ commands/test.js
@@ -1,14 +1,16 @@
-const { EmbedBuilder, SlashCommandBuilder } = require('discord.js');
+const { EmbedBuilder, SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
-        data: new SlashCommandBuilder()
-                .setName('test')
-                .setDescription('Test slash comamnds'),
-        async execute(interaction) {
-                        const embed = new EmbedBuilder()
-                                .setColor('#315D6E')
-                                .setTitle('Test embed!')
-                                .setDescription("Description")
-                        interaction.reply({ embeds: [embed] })
-        }
+  data: new SlashCommandBuilder()
+    .setName("test")
+    .setDescription("Test slash comamnds"),
+  async execute(interaction) {
+    const embed = new EmbedBuilder()
+      .setColor("#315D6E")
+      .setTitle("Test embed!")
+      .setDescription("Description");
+    interaction.reply({ embeds: [embed] }).catch((err) => {
+      console.log(err);
+    });
+  },
 };
