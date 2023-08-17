4fcdeba039f6f615061e190be9a606d99ca9af13
lun4h <71488561+piotr25691@users.noreply.github.com>
2023-08-17 14:25:28 +0200

    fixed script

--- main.js
+++ main.js
@@ -1,27 +1,23 @@
 // main.js
 
-const { Client } = require('discord.js');
-const client = new Client();
-require('dotenv').config();
+const { Client } = require("discord.js");
+const client = new Client({ intents: 33281 });
 
 // Event: Bot is ready
-client.once('ready', () => {
+client.once("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-// Event: !ceata
-client.on('message', (message) => {
-  if (message.content === '!ceata') {
-    message.reply('ok');
+// Event: !ceata, !open
+client.on("messageCreate", (message) => {
+  if (message.content === "!ceata") {
+    message.reply("ok");
+  }
+
+  if (message.content == "!open") {
+    message.reply("ok");
   }
 });
 
-// Event: !open
-client.on('message', (message) => {
-    if (message.content === '!open') {
-      message.reply('ok');
-    }
-  });
-
 // Login as the bot
-client.login('process.env.BOT_TOKEN');
+client.login(process.env.BOT_TOKEN);
