449121e6c5c3bd8cc388b19341a56b0538db4334
lun4h <71488561+piotr25691@users.noreply.github.com>
2023-10-06 12:19:19 +0200

    fix parameters

--- commands/ping_web.py
+++ commands/ping_web.py
@@ -6,21 +6,22 @@ import time
 plugin = lightbulb.Plugin(name="PingWeb")
 
 @plugin.command()
+@lightbulb.option("url", "The website to ping.", required=True, type=str)
 @lightbulb.command("pingweb", "ping a website and measure response time")
 @lightbulb.implements(lightbulb.SlashCommand)
-async def ping_website(ctx: lightbulb.Context, website_url: str):
+async def ping_website(ctx: lightbulb.Context):
     try:
         start_time = time.time()
         async with aiohttp.ClientSession() as session:
-            async with session.get(website_url) as response:
-                if response.status == 200:
+            async with session.get(ctx.options.url) as response:
+                if response.ok:
                     end_time = time.time()
                     response_time = (end_time - start_time).total_seconds() * 1000  # Convert to milliseconds
-                    await ctx.respond(f"website {website_url} is online. response time: {response_time:.2f}ms")
+                    await ctx.respond(f"Website `{ctx.options.url}` is online. response time: {response_time:.2f}ms")
                 else:
-                    await ctx.respond(f"website {website_url} returned status code {response.status}")
+                    await ctx.respond(f"Website `{ctx.options.url}` returned status code: {response.status}")
     except Exception as e:
-        await ctx.respond(f"an error occurred: {str(e)}")
+        await ctx.respond(f"An error occurred: {str(e)}")
 
 def load(bot):
     bot.add_plugin(plugin)
