87a8b62c039247ff5d20ea87e110ea1c5ed6cf9f
Xanudu <133562760+XanuduBuster@users.noreply.github.com>
2023-10-06 14:13:04 +0530

    make changea

--- commands/calculator.py
+++ commands/calculator.py
@@ -11,15 +11,15 @@ plugin = lightbulb.Plugin(name="Hello")
 async def calculator(ctx: lightbulb.Context):
     try:
         if ctx.options.expression in ["0/0", "0 / 0"]:
-            return await ctx.respond("Imagine that you have zero cookies and you split them evenly among zero friends. "
-                                     "How many cookies does each person get? See? It doesn't make sense. "
+            return await ctx.respond("imagine that you have zero cookies and you split them evenly among zero friends. "
+                                     "how many cookies does each person get? see? it doesn't make sense. "
                                      "And Cookie Monster is sad that there are no cookies, "
                                      "and you are sad that you have no friends.")
         result = eval(ctx.options.expression)
     except ZeroDivisionError:
         return await ctx.respond("Not Approved")
     except (SyntaxError, NameError):
-        return await ctx.respond("Please enter a valid expression like `/calc 3*23` or `/calc 34+35`")
+        return await ctx.respond("Please enter a valid expression like `/calc 69*69` or `/calc 69+69` or `/calc 69/14` or `/calc 69-14`")
     await ctx.respond(result)
 
 
