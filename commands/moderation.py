6ce797f9f786837c4d8b2f2917b6772c8751e6e6
Xanudu <133562760+XanuduBuster@users.noreply.github.com>
2023-10-06 21:32:05 +0530

    add moderation

---
+++ commands/moderation.py
@@ -0,0 +1,36 @@
+import hikari
+import lightbulb
+from lightbulb import checks
+import logging
+
+plugin = lightbulb.Plugin(name="Moderation")
+
+@plugin.command()
+@lightbulb.checks.has_guild_permissions(hikari.Permissions.BAN_MEMBERS)
+@lightbulb.command("ban", "Ban a user from the server")
+@lightbulb.implements(lightbulb.SlashCommand)
+async def ban(ctx: lightbulb.Context, member: hikari.User, *, reason: str = "No reason provided"):
+    try:
+        await ctx.guild.bans.create(member, reason=reason)
+        await ctx.respond(f"{member.username} has been banned from the server for {reason}")
+    except Exception as e:
+        logging.error(f"An error occurred while banning {member.username}: {e}")
+        await ctx.respond(f"An error occurred while banning {member.username}")
+
+@plugin.command()
+@lightbulb.checks.has_guild_permissions(hikari.Permissions.KICK_MEMBERS)
+@lightbulb.command("kick", "Kick a user from the server")
+@lightbulb.implements(lightbulb.SlashCommand)
+async def kick(ctx: lightbulb.Context, member: hikari.User, *, reason: str = "No reason provided"):
+    try:
+        await ctx.guild.kicks.create(member, reason=reason)
+        await ctx.respond(f"{member.username} has been kicked from the server for {reason}")
+    except Exception as e:
+        logging.error(f"An error occurred while kicking {member.username}: {e}")
+        await ctx.respond(f"An error occurred while kicking {member.username}")
+
+def load(bot):
+    bot.add_plugin(plugin)
+
+def unload(bot):
+    bot.remove_plugin(plugin)
