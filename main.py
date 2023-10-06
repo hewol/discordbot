08e4a5523dc239145ed8ea979b0ed1bb30484f5e
lun4h <71488561+piotr25691@users.noreply.github.com>
2023-10-06 13:17:06 +0200

    added cycling status

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
+import itertools
 
 import hikari
 import lightbulb
+from lightbulb.ext import tasks
 
 from dotenv import load_dotenv
 
@@ -12,11 +14,29 @@ bot = lightbulb.BotApp(
     intents=hikari.Intents.ALL_UNPRIVILEGED | hikari.Intents.MESSAGE_CONTENT,
 )
 
+statuses = itertools.cycle([
+    "/help\u30fbhttps://github.com/hewol/OpenBot",
+    "/help\u30fbhttps://github.com/hewol/aerOS"
+])
+
 
 @bot.listen(hikari.StartedEvent)
 async def on_ready(event: hikari.StartedEvent):
     print(f"Logged in as {str(bot.get_me())}!")
     bot.load_extensions_from("./commands", must_exist=True)
+    cycle_status.start()
+
+
+@tasks.task(s=30, pass_app=True)
+async def cycle_status(bot: lightbulb.BotApp):
+    # Cycling Statuses Task
+    await bot.update_presence(
+        status=hikari.Status("dnd"),
+        activity=hikari.Activity(
+            type=hikari.ActivityType.CUSTOM,
+            name=next(statuses),  # noqa
+        ),
+    )
 
 
 bot.run()
