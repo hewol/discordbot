6f4183d7636e7c2cffb1ec497058865b08a51210
lun4h <71488561+piotr25691@users.noreply.github.com>
2023-10-06 12:13:50 +0200

    xanudu put a tanjun context into lightbulb and the bot's now confused

--- commands/ping_web.py
+++ commands/ping_web.py
@@ -1,20 +1,20 @@
 import lightbulb
 import hikari
-import tanjun
 import aiohttp
+import time
 
-plugin = lightbulb.Plugin(name="Hello")
+plugin = lightbulb.Plugin(name="PingWeb")
 
 @plugin.command()
 @lightbulb.command("pingweb", "ping a website and measure response time")
-@lightbulb.implements(tanjun.SlashCommand)
-async def ping_website(ctx: tanjun.abc.Context, website_url: str):
+@lightbulb.implements(lightbulb.SlashCommand)
+async def ping_website(ctx: lightbulb.Context, website_url: str):
     try:
-        start_time = ctx.timestamp
+        start_time = time.time()
         async with aiohttp.ClientSession() as session:
             async with session.get(website_url) as response:
                 if response.status == 200:
-                    end_time = ctx.timestamp
+                    end_time = time.time()
                     response_time = (end_time - start_time).total_seconds() * 1000  # Convert to milliseconds
                     await ctx.respond(f"website {website_url} is online. response time: {response_time:.2f}ms")
                 else:
