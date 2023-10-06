1c2485feee3adda6be5df19e99f3fb71048efd90
Xanudu <133562760+XanuduBuster@users.noreply.github.com>
2023-10-06 14:20:32 +0530

    create ping command

---
+++ commands/ping.py
@@ -0,0 +1,20 @@
+import hikari
+import lightbulb
+
+plugin = lightbulb.Plugin(name="Hello")
+
+
+@plugin.command()
+@lightbulb.command("ping", "check the bot's ping")
+@lightbulb.implements(lightbulb.SlashCommand)
+async def ping(ctx: lightbulb.Context):
+    latency = round(ctx.bot.heartbeat_latency * 1000)
+    await ctx.respond(f"pong! Bot latency: {latency}ms")
+
+
+def load(bot):
+    bot.add_plugin(plugin)
+
+
+def unload(bot):
+    bot.remove_plugin(plugin)
