45bd44416bdfb5392459df65bb9776ef09b26dd7
Xanudu <133562760+XanuduBuster@users.noreply.github.com>
2023-10-06 14:14:02 +0530

    test

--- commands/test.py
+++ commands/test.py
@@ -5,11 +5,11 @@ plugin = lightbulb.Plugin(name="Hello")
 
 
 @plugin.command()
-@lightbulb.command("test", "Test slash commands")
+@lightbulb.command("test", "test slash commands")
 @lightbulb.implements(lightbulb.SlashCommand)
 async def test(ctx: lightbulb.Context):
-    e = hikari.Embed(title="Test embed!",
-                     description="Description",
+    e = hikari.Embed(title="test embed!",
+                     description="test",
                      color=hikari.Color.from_hex_code("#315d6e"))
     await ctx.respond(e)
 
