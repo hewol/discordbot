e4a53f7629caa2bb8ae56a2aa0c1232f47622f05
Piotr Zalewski <bardzoglodnypiotrek@gmail.com>
2023-10-07 14:00:12 +0200

    xanudu fails to define valid options

--- commands/moderation.py
+++ commands/moderation.py
@@ -7,29 +7,41 @@ plugin = lightbulb.Plugin(name="Moderation")
 
 
 @plugin.command()
+@lightbulb.option(
+    "member", "The person to ban", type=hikari.Member, required=True
+)
+@lightbulb.option(
+    "reason", "Reason for the ban", type=str, required=True
+)
 @lightbulb.add_checks(lightbulb.has_guild_permissions(hikari.Permissions.BAN_MEMBERS))
 @lightbulb.command("ban", "Ban a user from the server")
 @lightbulb.implements(lightbulb.SlashCommand)
-async def ban(ctx: lightbulb.Context, member: hikari.User, *, reason: str = "No reason provided"):
+async def ban(ctx: lightbulb.Context):
     try:
-        await ctx.get_guild().ban(member, reason=reason)
-        await ctx.respond(f"{member.username} has been banned from the server for: {reason}")
+        await ctx.get_guild().ban(ctx.options.member, reason=ctx.options.reason)
+        await ctx.respond(f"{ctx.options.member} has been banned from the server for: {ctx.options.reason}")
     except Exception as e:
-        logging.error(f"An error occurred while banning {member.username}: {e}")
-        await ctx.respond(f"An error occurred while banning {member.username}")
+        logging.error(f"An error occurred while banning {str(ctx.options.member)}: {e}")
+        await ctx.respond(f"An error occurred while banning {str(ctx.options.member)}")
 
 
 @plugin.command()
+@lightbulb.option(
+    "member", "The person to kick", type=hikari.Member, required=True
+)
+@lightbulb.option(
+    "reason", "Reason for the kick", type=str, required=True
+)
 @lightbulb.add_checks(lightbulb.has_guild_permissions(hikari.Permissions.KICK_MEMBERS))
 @lightbulb.command("kick", "Kick a user from the server")
 @lightbulb.implements(lightbulb.SlashCommand)
-async def kick(ctx: lightbulb.Context, member: hikari.User, *, reason: str = "No reason provided"):
+async def kick(ctx: lightbulb.Context):
     try:
-        await ctx.get_guild().kick(member, reason=reason)
-        await ctx.respond(f"{member.username} has been kicked from the server for: {reason}")
+        await ctx.get_guild().kick(ctx.options.member, reason=ctx.options.reason)
+        await ctx.respond(f"{str(ctx.options.member)} has been kicked from the server for: {ctx.options.reason}")
     except Exception as e:
-        logging.error(f"An error occurred while kicking {member.username}: {e}")
-        await ctx.respond(f"An error occurred while kicking {member.username}")
+        logging.error(f"An error occurred while kicking {str(ctx.options.member)}: {e}")
+        await ctx.respond(f"An error occurred while kicking {str(ctx.options.member)}")
 
 
 def load(bot):
